786caffcd8ac11efac77c2a1435fab763c453726
Isaac <izaak343@gmail.com>
2019-01-28 19:24:30 -0600

    Misc

---
+++ OGU/unit1_tst.py
@@ -0,0 +1,12 @@
+class unit():
+
+    def __init__(self):
+        self.unit_name = 'Droid'
+
+    def unit_values(self):
+        self.hitpoints = 100
+        self.armor = 3
+        self.attak = 6
+        self.speed = 5
+        self.min_range = 0
+        self.max_range = 1
---
+++ matrix/matrix_product.py
@@ -0,0 +1,4 @@
+matrix_1_x = int(input('Matrix one height: '))
+matrix_2_y = int(input('Matrix one with: '))
+matrix_2_x = int(input('Matrix two height: '))
+matrix_2_y = int(input('Matrix two with: '))
